55dc23ef3f31e0100c5b36e5b3984eba96410058
JWNicholson <john.nicholson812@gmail.com>
2020-08-05 17:30:59 -0400

    working on word_count. Code is getting complex. Looking at alternative

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,15 @@
 def word_count(s):
     # Your code here
+#input string
+#ignore certain characters
+#count how many times word occurs
+#build a dictionary
+#if its already in there increment count by 1
 
+    #split() s on spaces
+    inputStr = s.split("")
+    ignore_char = set(['"', ':', ';', ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&'])
+print(s)
 
 
 if __name__ == "__main__":
