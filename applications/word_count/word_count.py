a1ab11c0908036bb96fa8905da7ecb8d99e65ce8
JWNicholson <john.nicholson812@gmail.com>
2020-08-05 18:04:56 -0400

    finished word_count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -6,10 +6,38 @@ def word_count(s):
 #build a dictionary
 #if its already in there increment count by 1
 
-    #split() s on spaces
-    inputStr = s.split("")
-    ignore_char = set(['"', ':', ';', ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&'])
-print(s)
+    words = s.lower()
+
+    #convert whitespace characters to a space
+    char_conv = '\n \t \r'.split(" ")
+
+    for whitespace in char_conv:
+        words = words.replace(whitespace, " ")
+
+    #remove ignored characters & split
+    ignore_char = '" : ; , . - + = / \ | [ ] { } ( ) * ^ &'.split(" ")
+
+    for c in ignore_char:
+        words = words.replace(c, "")
+
+    #convert string to array of words
+    words = words.split(" ")
+
+    words_dict = dict()
+    #skip over any empty strings
+    for word in words:
+        if word == "":
+            continue
+        #count occurances of word
+        if word in words_dict:
+            # there's another one or more
+            words_dict[word] +=1
+            
+        else:# its the only one of its kind
+            words_dict[word] = 1
+
+    return words_dict
+
 
 
 if __name__ == "__main__":
