9fa128389135b2a99e58871a522eb07134f00294
JWNicholson <john.nicholson812@gmail.com>
2020-08-05 18:29:32 -0400

    no_dups complete

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,20 @@
 def no_dups(s):
     # Your code here
+    #split into a list on whitespaces
+    words = s.split(" ")
 
+    words_dict = dict()
+    dups_removed = []
+
+    for word in words:
+        # check if its already in there
+        if word not in words_dict:
+            # Its not --
+            words_dict[word] = 1
+            dups_removed.append(word)
+            
+    # put all items into one string
+    return " ".join(dups_removed)
 
 
 if __name__ == "__main__":
