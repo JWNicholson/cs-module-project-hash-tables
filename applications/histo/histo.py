1c75e8e9fbf948714beb33c4da918f335dca863f
JWNicholson <john.nicholson812@gmail.com>
2020-08-06 16:45:42 -0400

    markov and histo complete

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,81 @@
 # Your code here
+#https://docs.python.org/3/howto/sorting.html
+#https://www.geeksforgeeks.org/ways-sort-list-dictionaries-values-python-using-itemgetter/
+#https://docs.python.org/3/library/operator.html
+
+from operator import itemgetter
+
+
+def word_count(s):
+    # Your code here
+#input string
+#ignore certain characters
+#count how many times word occurs
+#build a dictionary
+#if its already in there increment count by 1
+
+    words = s.lower()
+
+    #convert whitespace characters to a space
+    char_conv = '\n \t \r'.split(" ")
+
+    for whitespace in char_conv:
+        words = words.replace(whitespace, " ")
+
+    #remove ignored characters & split
+    ignore_char = '" : ; , . - + = / \ | [ ] { } ( ) * ^ &'.split(" ")
+
+    for c in ignore_char:
+        words = words.replace(c, "")
+
+    #convert string to array of words
+    words = words.split(" ")
+
+    words_dict = dict()
+    #skip over any empty strings
+    for word in words:
+        if word == "":
+            continue
+        #count occurances of word
+        if word in words_dict:
+            # there's another one or more
+            words_dict[word] +=1
+            
+        else:# its the only one of its kind
+            words_dict[word] = 1
+
+    return words_dict
+
+#make the histogram
+def make_histogram(file_path):
+#open and read the file
+    with open(file_path) as file:
+        text = file.read()
+
+
+#     #create a dictionary from word_count, storing occurances of words
+    freq = word_count(text)
+
+#     #convert dictionary to a list and sort number of occurances in  descending order
+    freq_list = freq.items()
+    freq_list = sorted(freq_list, key=itemgetter(1, 0), reverse=True)
+
+#     #find the longest word
+    longest_word = len(freq_list[0][0])
+
+    for word_data in freq_list:
+        if len(word_data[0]) > longest_word:
+            longest_word = len(word_data[0])
+
+#     #return words and occurances, with one # from each occurance
+    for word_data in freq_list:
+        format_word = word_data[0] + (longest_word - len(word_data[0])) * " "
+        histogram_bar = "#" * word_data[1]
+
+        histogram_row = format_word + "  " + histogram_bar
+
+        print(histogram_row)
+
+make_histogram("robin.txt")
+
 
--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,13 +1,78 @@
 import random
 
+#Read the input file and split into words
+
+#Analyze the text to build up the dataset
+## Which wirds can follow a word - andy that actually does
+## any word at index + 1
+## How to build a dataset?
+
+###Use a hashtable
+## #relate on piece of info with other info
+### Requent lookups
+### key:word, value: list of all words that follow this word
+
+
+#Choose a random "start word" to begin (Starts with capitol letter or quotation mark)
+## First or second character is capitalized
+
+## Make a list of start words
+
+#Loop over, choose a random following word. If its a stop word
+##Stop word - ends with a period, question mark, ..., etc
+
+###Use a hash table
+
 # Read in all the words in one go
-with open("input.txt") as f:
+with open("/mnt/h/CS32/Hashtables/cs-module-project-hash-tables/applications/markov/input.txt") as f:
     words = f.read()
 
-# TODO: analyze which words can follow other words
-# Your code here
+# # TODO: analyze which words can follow other words
+# # Your code here
 
+#split words on spaces
+words = words.split()
+# Keep track of all the words that can follow a word
+following_words = {}  
 
-# TODO: construct 5 random sentences
-# Your code here
+prev = None
 
+for w in words:
+    if prev is not None:
+
+        # Make an empty list for the first entries
+        if prev not in following_words:
+            following_words[prev] = []
+
+        # Add word to the list of those that are following_words
+        following_words[prev].append(w)
+
+    prev = w
+
+# #Make a list of start words
+# Check if word starts with a capital letter
+is_start = lambda x: x[0].isupper() or len(x) > 1 and x[1].isupper()
+start_words = [w for w in following_words.keys() if is_start(w)]
+
+# # TODO: construct 5 random sentences
+# # Your code here
+# Print 5 of paragraphs
+for _ in range(5):
+
+    # Choose the starting word
+    w = random.choice(start_words)
+
+    stopped = False
+    stop_sign = ".!?"  # Stop on any of these punctuation marks
+
+    while not stopped:
+        print(w, end=" ")
+        # condition of ? at end of sentence or someting like !" or any word longer than 1 element
+        if w[-1] in stop_sign or len(w) > 1 and w[-2] in stop_sign:
+            stopped = True
+        else:
+            # Follow to the next word in the chain
+            next_words = following_words[w]
+            w = random.choice(next_words)
+
+    print("\n")
